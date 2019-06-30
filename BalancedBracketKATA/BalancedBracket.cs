765105f30e2794a33d8911d8d71124759fa36673
Andres Flores <floresc.andres@gmail.com>
2019-06-30 13:00:52 -0400

    Add project files.

---
+++ BalancedBracketKATA/BalancedBracket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBracketKATA
+{
+    class BalancedBracket
+    {
+       public string RunBalancedBracket(string input)
+        {
+            int strLength = input.Length - 1;
+
+            if (input == "")
+            {
+                return string.Empty;
+            }
+            else if (input[0] == '[' && input[strLength] == ']' && input.Length % 2 == 0)
+            {
+                if (input.Length > 2)
+                {
+                    for (int i = 1; i < strLength; i++)
+                    {
+                        if (input[i] == input[strLength - i])
+                        {
+                            return "FAIL";
+                        }
+                    }
+                }
+
+                return "OK";
+            }
+            
+            return "FAIL";
+        }
+    }
+}
---
+++ BalancedBracketKATA/BalancedBracketTest.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBracketKATA
+{
+    [TestFixture]
+    public class BalancedBracketTest
+    {
+        [Test]
+        public void ReturnEmptyString()
+        {
+            string input = "";
+            string expected = "";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnOKWhenSentABalancedBracket()
+        {
+            string input = "[]";
+            string expected = "OK";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnFAILWhenSentAnUnbalancedBracket()
+        {
+            string input = "][";
+            string expected = "FAIL";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnOKWhenSentAStringWithEvenLength()
+        {
+            string input = "[[]]";
+            string expected = "OK";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnFAILWhenSentAStringWithoutBalancedBrackets()
+        {
+            string input = "[[[]";
+            string expected = "FAIL";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnOKWhenSentALongStringWithBalancedBrackets()
+        {
+            string input = "[[][][]]";
+            string expected = "OK";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnFAILWhenSentALongStringWithoutBalancedBrackets()
+        {
+            string input = "[[[]]][]";
+            string expected = "FAIL";
+            var initialize = new BalancedBracket();
+            string result = initialize.RunBalancedBracket(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
